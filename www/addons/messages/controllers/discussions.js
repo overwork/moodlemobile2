952f46fe839e50fe3a4dbf35da15f631439fc7d3
brikend <brikend.rama@hig.no>
2016-11-18 13:54:55 +0100

    Implementation of unread message count in discussions. Assynchronous calls still not fixed.

--- www/addons/messages/controllers/discussions.js
+++ www/addons/messages/controllers/discussions.js
@@ -32,13 +32,30 @@ angular.module('mm.addons.messages')
     function fetchDiscussions() {
         return $mmaMessages.getDiscussions().then(function(discs) {
             discussions = discs;
-
             // Convert to an array for sorting.
             var array = [];
+            console.log(discussions);
+
             angular.forEach(discussions, function(v) {
+                v.unreadMsgs = 0;
+
+                console.log('Going THROUGH messages');
+                $mmaMessages.getDiscussion(v.message.user).then(function(messages) {
+                    console.log('PRINTING MESSAGES');
+                    console.log(messages);
+                    angular.forEach(messages, function(msg) {
+                        if (msg.read == 0) {
+                            console.log('found one unread message');
+                            v.unreadMsgs += 1;
+                        }
+                    });
+                });
+
                 array.push(v);
             });
             $scope.discussions = array;
+            console.log('PRINTING DISCUSSIONS AFTER')
+            console.log($scope.discussions);
         }, function(error) {
             if (typeof error === 'string') {
                 $mmUtil.showErrorModal(error);
@@ -55,6 +72,7 @@ angular.module('mm.addons.messages')
     }
 
     $scope.refresh = function() {
+        console.log('SCOPE  REFRESH');
         refreshData().finally(function() {
             $scope.$broadcast('scroll.refreshComplete');
         });
@@ -89,6 +107,7 @@ angular.module('mm.addons.messages')
 
     $scope.$on('$destroy', function() {
         if (newMessagesObserver && newMessagesObserver.off) {
+            console.log('DESTROY TRUE');
             newMessagesObserver.off();
         }
     });
